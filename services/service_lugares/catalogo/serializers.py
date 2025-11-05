99217b7675ca730bc67b7c74b619bb7d96c28f05
Teodosio Donaire González <teodonaire@gmail.com>
2025-11-05 02:20:12 +0100

    Crea el LugarSerializer para la app catalogo

---
+++ services/service_lugares/catalogo/serializers.py
@@ -0,0 +1,44 @@
+# services/service_lugares/catalogo/serializers.py
+
+from rest_framework import serializers
+from .models import Lugar, Categoria, EstadoAprobacion
+
+class LugarSerializer(serializers.ModelSerializer):
+    """
+    Este 'traductor' convierte el modelo Lugar a JSON
+    y valida los datos de entrada.
+    """
+
+    categoria_nombre = serializers.CharField(source='get_categoria_display', read_only=True)
+    estado_nombre = serializers.CharField(source='get_estado_display', read_only=True)
+
+    esta_aprobado = serializers.ReadOnlyField()
+    es_visible = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Lugar
+        fields = [
+            'id', 
+            'nombre', 
+            'descripcion', 
+            'direccion', 
+            'lat', 
+            'lng', 
+            'categoria',            
+            'categoria_nombre',     
+            'estado',               
+            'estado_nombre',        
+            'creado_por_id',        # El ID del usuario que lo creó
+            'creado_en',
+            'esta_aprobado',
+            'es_visible',
+        ]
+
+        # --- Campos de Solo Lectura (para la salida) ---
+        read_only_fields = [
+            'estado',
+            'estado_nombre',
+            'creado_en',
+            'esta_aprobado',
+            'es_visible',
+        ]
