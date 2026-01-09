adfcc4d84487363c51fd361d1c0dfbc4e4067b78
Teodosio Donaire González <teodonaire@gmail.com>
2026-01-09 09:32:43 +0100

    Modificar admin.py

--- services/service_usuarios/usuarios/admin.py
+++ services/service_usuarios/usuarios/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Usuario, Perfil
+from .models import Usuario
+class CustomUserAdmin(UserAdmin):
+    # Añadimos tus campos personalizados al formulario de edición
+    fieldsets = UserAdmin.fieldsets + (
+        ('Información Extra de CultureMap', {'fields': ('rol', 'biografia', 'puntos')}),
+    )
+    
+    # Columnas que se verán en la lista de usuarios
+    list_display = ('username', 'email', 'first_name', 'last_name', 'rol', 'puntos', 'is_staff')
+    
+    # Filtros laterales
+    list_filter = UserAdmin.list_filter + ('rol',)
 
-admin.site.register(Usuario, UserAdmin)
-admin.site.register(Perfil)
+# Registramos solo el Usuario con la configuración personalizada
+admin.site.register(Usuario, CustomUserAdmin)
